c919bedc50b6c46c121d0fa65a49fb61042712ab
NikitaKazancev <nkaz2003@gmail.com>
2023-02-12 12:48:35 +0300

    branch1-second

--- src/Camera.tsx
+++ src/Camera.tsx
@@ -15,7 +15,7 @@ import type { CameraVideoCodec, RecordVideoOptions, VideoFile } from './VideoFil
 export type CameraPermissionStatus = 'authorized' | 'not-determined' | 'denied' | 'restricted';
 export type CameraPermissionRequestResult = 'authorized' | 'denied';
 
-interface ITest {
+interface ITest1 {
   color: string
 }
 
